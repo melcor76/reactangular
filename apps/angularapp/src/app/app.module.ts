a3e4e8e148bd6c66927a575515bd7bc46683c86d
Michael Karén <michael.karen@gmail.com>
2019-07-07 15:07:35 +0200

    navigation

--- apps/angularapp/src/app/app.module.ts
+++ apps/angularapp/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
--- apps/reactapp/src/app/app.tsx
+++ apps/reactapp/src/app/app.tsx
@@ -4,32 +4,15 @@ import './app.css';
 
 export const App = () => {
   return (
-    <div>
-      <header style={{ textAlign: 'center' }}>
-        <h1>Welcome to reactapp!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/nx-logo.png"
-        />
-      </header>
-      <p>
-        This is a React app built with <a href="https://nx.dev">Nx</a>.
-      </p>
-      <p>🔎 **Nx is a set of Angular CLI power-ups for modern development.**</p>
-      <h2>Quick Start & Documentation</h2>
-      <ul>
-        <li>
-          <a href="https://nx.dev/getting-started/what-is-nx">
-            30-minute video showing all Nx features
-          </a>
-        </li>
-        <li>
-          <a href="https://nx.dev/tutorial/01-create-application">
-            Interactive tutorial
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <img src="../assets/react.png" alt="React" width="40" height="40" />
+      <a href="http://localhost:4200">
+        <img src="../assets/angular.png" alt="Angular" width="40" height="40" />
+      </a>
+      <div>
+        <img src="../assets/react.png" alt="Avatar" width="300" />
+      </div>
+    </>
   );
 };
 
