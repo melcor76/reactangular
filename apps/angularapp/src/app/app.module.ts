611466590e1414e86a61aecd94d92cbfb73b31c1
Michael Karén <michael.karen@gmail.com>
2019-07-07 15:18:36 +0200

    remove material

--- apps/angularapp/src/app/app.module.ts
+++ apps/angularapp/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
+  imports: [BrowserModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
--- apps/reactapp/src/app/app.tsx
+++ apps/reactapp/src/app/app.tsx
@@ -5,10 +5,10 @@ import './app.css';
 export const App = () => {
   return (
     <>
-      <img src="../assets/react.png" alt="React" width="40" height="40" />
       <a href="http://localhost:4200">
         <img src="../assets/angular.png" alt="Angular" width="40" height="40" />
       </a>
+      <img src="../assets/react.png" alt="React" width="40" height="40" />
       <div>
         <img src="../assets/react.png" alt="Avatar" width="300" />
       </div>
