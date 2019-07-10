cec42373895149bf0aabdc8885b1c4c769e57c8d
Michael Karén <michael.karen@gmail.com>
2019-07-10 22:12:31 +0200

    event listener in react

--- apps/reactapp/src/app/app.tsx
+++ apps/reactapp/src/app/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './app.css';
 
@@ -7,6 +7,14 @@ const divStyle = {
 };
 
 export const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.querySelector('#addButton').addEventListener('addToCart', e => {
+      setCounter(counter + 1);
+    });
+  });
+
   return (
     <>
       <header className="header">
@@ -21,11 +29,11 @@ export const App = () => {
           </a>
           <img src="../assets/react.png" alt="React" width="40" height="40" />
         </div>
-        <shopping-cart counter={0} />
+        <shopping-cart counter={counter} />
       </header>
       <div>
         <img src="../assets/react.png" alt="Avatar" width="300" />
-        <add-to-cart-button type="React" />
+        <add-to-cart-button id="addButton" type="React" />
       </div>
     </>
   );
