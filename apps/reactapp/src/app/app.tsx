29b2aa7f76f19d33eb718d0adf69e68a076f99f8
Michael Karén <michael.karen@gmail.com>
2019-07-22 00:22:00 +0200

    class to function component

--- apps/reactapp/src/app/app.tsx
+++ apps/reactapp/src/app/app.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import '@reactangular/elements';
 import './app.css';
 
@@ -10,67 +10,58 @@ interface MyState {
   counter: number;
 }
 
-class App extends Component<{}, MyState> {
-  private buttonRef;
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.buttonRef = React.createRef();
-  }
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const buttonRef = useRef(null);
 
-  componentDidMount() {
-    this.buttonRef.current.addEventListener('addToCart', () =>
-      this.setState({ counter: this.state.counter + 1 })
-    );
-  }
+  useEffect(() => {
+    const handleEvent = () => setCounter(counter + 1);
+    buttonRef.current.addEventListener('addToCart', handleEvent);
+    return () => {
+      buttonRef.current.removeEventListener('addToCart', handleEvent);
+    };
+  }, [counter]);
 
-  componentWillUnmount() {
-    this.buttonRef.current.removeEventListener('addToCart');
-  }
-
-  render() {
-    const { counter } = this.state;
-    return (
-      <>
-        <header className="flex-row">
-          <div style={divStyle}>
-            <a href="http://localhost:4200">
-              <img
-                src="../assets/angular.png"
-                alt="Angular"
-                width="40"
-                height="40"
-              />
-            </a>
-            <img src="../assets/react.png" alt="React" width="40" height="40" />
-          </div>
-          <shopping-cart counter={counter} />
-        </header>
-        <div className="flex-row">
-          <img src="../assets/react.png" alt="Avatar" width="150" />
-          <div>
-            <table>
-              <thead>
-                <tr>
-                  <th>Product</th>
-                  <th>Type</th>
-                  <th>Features</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>React</td>
-                  <td>Library</td>
-                  <td>JSX, Hooks</td>
-                </tr>
-              </tbody>
-            </table>
-            <add-to-cart-button type="React" ref={this.buttonRef} />
-          </div>
+  return (
+    <>
+      <header className="flex-row">
+        <div style={divStyle}>
+          <a href="http://localhost:4200">
+            <img
+              src="../assets/angular.png"
+              alt="Angular"
+              width="40"
+              height="40"
+            />
+          </a>
+          <img src="../assets/react.png" alt="React" width="40" height="40" />
         </div>
-      </>
-    );
-  }
-}
+        <shopping-cart counter={counter} />
+      </header>
+      <div className="flex-row">
+        <img src="../assets/react.png" alt="Avatar" width="150" />
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Type</th>
+                <th>Features</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>React</td>
+                <td>Library</td>
+                <td>JSX, Hooks</td>
+              </tr>
+            </tbody>
+          </table>
+          <add-to-cart-button type="React" ref={buttonRef} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
